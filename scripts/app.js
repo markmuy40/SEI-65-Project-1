02d7ca1c05648fac23c497fe1f73da2a806cc0db
Mark Muyuela <mark.muyuela81@gmail.com>
2022-07-16 13:39:05 +0100

    HTML framework added

---
+++ scripts/app.js
@@ -0,0 +1,177 @@
+function init() {
+
+  // ! make the grid  
+
+  // Grid container
+
+  // ! aliens
+
+  // const alienClass = 'alien'// alienclass in CSS
+  // const startPosition = (numerical number for bottom/middle of grid)
+  // let currentPosition = startPosition
+  // //use setInterval for movement
+  
+  // function addAlien(position){
+  // cells[position].classList.add(alienClass)
+  // }
+  
+  // function removeAlien(position){
+  // cells[position].classList.remove(alienClass)
+  // }
+  // removeAlien()
+  
+  
+  // // ! function for left movement
+  // function alienLeft(){
+  // alien.classList.remove()  -1 alien.ClassList.add()
+  // }
+  
+  // // ! function for right movement
+  // function alienRight(){
+  // alien.classList.remove()  +1 alien.ClassList.add()
+  // }
+  
+  // function for moving downward
+  // function alienDown(){
+  // removeAlien(currentPosition)  +10 addAlien(CurrentPosition)
+  // }
+  
+  // ! removing aliens
+
+  // function deadAlien() {
+  // If (alien currentPosition === shot currentPosition){
+  // removeAlien() + (add empty element to keep spacing
+  // score.innerHTML = score + 10 (whatever value a kill is worth)
+  // } else {
+  // shot -= 10 ( whatever width the grid is)
+  // }
+  
+  
+  
+  // alien collision -
+  // if (alien === player) {
+  // lives -=1 
+  // } else if {
+  // (alien ===  cell.length - 9)
+  // return gameOver()
+  // }
+  
+  // function addAlien(position){
+  // cells[position].classList.add(alien)
+  // }
+
+  // function removeAlien(position){
+  // cells[position].classList.remove(alien)
+  // }
+  
+  // detect when they hit the end move them 
+  // down. switch direction
+  
+  
+  
+  //! player functions
+  // // variables
+  // const playerClass = 'player' 
+  // const startPosition = (numerical number for bottom/middle of grid)
+  // let currentPosition = startPosition
+  
+  // function addPlayer(position){
+  // cells[position].classList.add(playerClass)
+  // }
+  
+  // function removePlayer(position){
+  // cells[position].classList.remove(playerClass)
+  // }
+  
+  // function playerMovement(){
+  
+  // ! variables
+  // const left = keycode || left = keycode || left = div class
+  // const right = keycode  || right = keycode number || right = div class
+  
+  // removePlayer(currentPosition)
+  // execution
+  // if (left === keycode && currentPosition % width !== 0) {
+  // currentPosition -= 1
+  // } else if {
+  // right === keycode && currentPosition % width !== width - 1) {
+  // currentPosition += 1
+  //   }
+  //   addPlayer(position)
+  // }
+  // .addEventListener('keydown', playerMovement)
+  
+  
+  
+  // ! firing
+  
+  // Const shotClass = 'shot' use image/icon
+  
+  // function addShot(position){
+  // cells[position].classList.add(shotClass)
+  // }
+  // function removeAlien(position){
+  // cells[position].classList.remove(shotClass)
+  // }
+  
+  // function playerShoot(){
+  // const fire = (keycode) || fire = div class
+  
+  // shotTime = setInterval(() => {
+  
+  // shot = player.classList.add() + 10 
+  // if(shot < (bottom row lowest number) {
+  // shot += 10
+  // } else {
+  // return deadAlien()
+  //   },  1000)
+  // }
+  
+  // document.addEventListener('keyup', playerShoot)
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
