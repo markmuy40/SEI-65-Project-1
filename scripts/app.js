467d28fbef436f89fa79ac5a441b3ea2ddfa2ce5
Mark Muyuela <mark.muyuela81@gmail.com>
2022-07-24 18:42:47 +0100

    resized to fit full height - no scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,25 @@
 function init() {
 
   // ! DOM Elements =======================================================================
-  
   const start = document.querySelector('.play')
-  //console.log(start)
   const reset = document.querySelector('.reset')
-  //console.log(reset)
   const scoreDisplay = document.querySelector('.showscore')
-  //console.log(scoreDisplay)
   const livesDisplay = document.querySelector('.showlives')
-  //console.log(livesDisplay)
+
   const grid = document.querySelector('.grid')
-  //console.log(grid)
   const audio1 = document.querySelector('.audio1')
-  //console.log(audio1)
   const audio2 = document.querySelector('.audio2')
-  //console.log(audio2)
   const audio3 = document.querySelector('.audio3')
-  //console.log(audio3)
   const audio4 = document.querySelector('.audio4')
-  //console.log(audio4)
   const audio5 = document.querySelector('.audio5')
-  //console.log(audio5)
-  // multiple audio means that SFX aren't chopped or removed as each audio 
-  // function has a specific channel
+
   
   // ! Global variables ====================================================================
 
   // ! grid variables
-  const width = 10 //change number if you want to add cells. 
-  //change percentages of .grid div as well
+  const width = 10
   const cellCount = width * width
-  const cells = []// targetting each div cell created 
+  const cells = [] 
   
   // ! score variables
   let score = 0
@@ -53,18 +41,18 @@ function init() {
 
   // ! Zombie variables
   let zombies = [1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18,21,22,23,24,25,26,27,28]  
-  //create zombie array. Number should match up to squares in grid.
   const zombieClass = 'zombie'
   const biteClass = 'bite'
   let zombieTimer
   let bombTimer1
+
+  // ! bomb variables
+  const bombClass = 'bomb'
   
   // ! make the grid  =======================================================================
   function makeGrid(){
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
-      //cell.innerText = i //remove once happy with design and no longer need to crate more 
-      // functions for movement.
       cell.dataset.index = i
       cells.push(cell)
       grid.appendChild(cell)
@@ -93,13 +81,12 @@ function init() {
     const leftAlt = 65 
     const right = 39
     const rightAlt = 68
-    //console.log(keyCode)
+
     
     removePlayer(currentPosition)
 
     if ((left === keyCode || leftAlt === keyCode) && currentPosition % width !== 0) {
       currentPosition -= 1
-      //console.log('left move')
     } else if ((right === keyCode || rightAlt === keyCode) && currentPosition % width !== width - 1){
       currentPosition += 1
     }
@@ -136,14 +123,10 @@ function init() {
           removeShot(shotPosition)
           shotPosition -= width
           if (zombies.includes(shotPosition) === true) {
-            // find index of zombie hit in the array
             zombieHitAudio()
             score += 100
             scoreDisplay.innerHTML = score
-            //find the index of the zombie and save as variable
             const collision = zombies.find(zombie => zombie === shotPosition)
-            //console.log('collision', collision)
-            //removes zombie index from the zombie array
             zombies.splice(zombies.indexOf(collision), 1)
             removeShot(shotPosition)
             cells[shotPosition].classList.add(hitClass)
@@ -163,9 +146,7 @@ function init() {
   
   // ! Zombie functions ====================================================================
 
-  //create zombie array into grid
   function createZombies(){
-    //'for each' zombie in zombies array, add zombie class into cell with index of each zombie.
     deleteZombies()
     zombies.forEach(zombie => {
       cells[zombie].classList.add(zombieClass)
@@ -181,21 +162,18 @@ function init() {
   function zombiesLeft(){
     deleteZombies()
     zombies = zombies.map(alien => alien - 1)
-    //console.log('going left', zombies)
     createZombies()
   }
   
   function zombiesRight(){
     deleteZombies()
     zombies = zombies.map(alien => alien + 1)
-    //console.log('going right', zombies)
     createZombies()
   }
   
   function zombiesDown(){
     deleteZombies()
     zombies = zombies.map(zombie => zombie + width)
-    //console.log('going down', zombies)
     createZombies()
   }
 
@@ -203,11 +181,9 @@ function init() {
   
   function zombiesMove(){
     zombieTimer = setInterval(() =>{
-      //console.log('movement', aliens)
       const rowIsEven = Math.floor(zombies[0] / 10) % 2 === 0
       if (rowIsEven && zombies[zombies.length - 1] % width !== width - 1){
         zombiesRight()
-        //console.log(aliens)
       } else if (!rowIsEven && zombies[0] % width !== 0) {
         zombiesLeft()
       } else {
@@ -220,15 +196,12 @@ function init() {
         clearInterval(zombieTimer)
       } else if (!zombies.length === true) {
         clearInterval(zombieTimer)
-        //console.log(!zombies.length)
-        //console.log('after statement')
         return winner()
       }
     }, 800) 
   }
- 
+
   // ! zombie projectile ==================================================================== 
-  const bombClass = 'bomb'
   
   function addBomb(bombPosition){
     if (bombPosition < 99) cells[bombPosition].classList.add(bombClass)
@@ -245,15 +218,12 @@ function init() {
       const randomIndex = Math.floor(Math.random() * (lastFour.length))
       let bombPosition = zombies[randomIndex]
       console.log(zombies[randomIndex])
-      zombieBomb() // sfx
+      zombieBomb()
       const bombTimer2 = setInterval(() => { 
         removeBomb(bombPosition)
         bombPosition += width
         if (bombPosition < (width * width) - 1) {          
-          //console.log('bomb', bombPosition)
-          //console.log('player', currentPosition)
           if (bombPosition === currentPosition) {
-            //console.log('if statement triggered')
             ouch()
             lives -= 1
             livesDisplay.innerHTML = lives ? '❤️'.repeat(lives) : '💔'
@@ -326,10 +296,12 @@ function init() {
     deleteZombies()
     clearInterval(zombieTimer)
   }
+
   // ! game over
   function gameOver(){
     setTimeout(() => alert(`You scored ${score} points!`), 200)
     deleteZombies()
+    removeBomb()
     clearInterval(zombieTimer)
   }
 
